8b729e30e723259a7f544fcb524e6d5dab55f930
ASUS1 <melisaliyazicioglu@hotmail.com>
2018-06-12 16:49:34 +0300

    yeni

--- src/main/java/onduty/Message.java
+++ src/main/java/onduty/Message.java
@@ -3,11 +3,15 @@ package onduty;
 public class Message {
 	
 	private long messsageid;  //bigint--> long
-	private int messagestate;
-	private int state;
+	private int messagestate;  // mesajýn alýnýp alýnmadýðý
+
 	private String messagecontent;  // text veri tipi nasýl tanýmlanýr?
 	private long conversationid;  //foreign key
+
+//	@Column(name="messagestate")
+	private MessageState messageState;
 	
 	
+	private State state;   //annotation
 
 }
