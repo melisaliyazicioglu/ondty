bf05930efc98b577bf68c3979e5f47e7c36e4ff2
ASUS1 <melisaliyazicioglu@hotmail.com>
2018-06-12 16:35:14 +0300

    message içeriği

--- src/main/java/onduty/Message.java
+++ src/main/java/onduty/Message.java
@@ -1,5 +1,13 @@
 package onduty;
 
 public class Message {
+	
+	private long messsageid;  //bigint--> long
+	private int messagestate;
+	private int state;
+	private String messagecontent;  // text veri tipi nasýl tanýmlanýr?
+	private long conversationid;  //foreign key
+	
+	
 
 }
