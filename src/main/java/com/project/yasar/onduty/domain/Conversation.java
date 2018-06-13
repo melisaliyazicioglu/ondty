1e2ebb776c47fa848576ab8f3a93edf930b55ed0
ASUS1 <melisaliyazicioglu@hotmail.com>
2018-06-13 15:03:33 +0300

    ML

--- src/main/java/com/project/yasar/onduty/domain/Conversation.java
+++ src/main/java/com/project/yasar/onduty/domain/Conversation.java
@@ -1,6 +1,27 @@
 package com.project.yasar.onduty.domain;
 
+import java.util.Date;
+
+import javax.persistence.*;
+
 public class Conversation {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;  //conversation_id
+	
+	@Column
+	private Date conversationdate;
+
+	public Conversation(Date conversationdate) {
+		super();
+		this.conversationdate = conversationdate;
+	}
+	
+	//foreign key yok
+	
+	
+	
 
 	
 }
