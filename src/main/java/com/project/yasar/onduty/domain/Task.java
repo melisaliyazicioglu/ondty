3c8f9178fbc157d9a2e9214526da059f27715432
özge <zge@Lenovo-PC>
2018-06-13 16:23:53 +0300

    deneme2

--- src/main/java/com/project/yasar/onduty/domain/Task.java
+++ src/main/java/com/project/yasar/onduty/domain/Task.java
@@ -1,5 +1,34 @@
 package com.project.yasar.onduty.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
 public class Task {
 
+	 @Id   
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    private long id;
+	 @Column
+	    private String name;
+	 @Column
+	 
+	 @Enumerated(EnumType.ORDINAL)
+	    private TaskStateType taskstatetype; 
+	
+	 @Column
+	 private String assignerpersonal;
+	
+	 @Column
+	    @Enumerated(EnumType.ORDINAL)
+	    private TaskPriority taskpriority; 
+	 
+	 @JoinColumn(name = "projectid")
+	    private Project project;   
+	 //baðlantý yapýlcak 
+	 @Column
+	 private Date taskstartdate;
+	 @Column
+	 private Date taskenddate;
+	 
 }
+	
